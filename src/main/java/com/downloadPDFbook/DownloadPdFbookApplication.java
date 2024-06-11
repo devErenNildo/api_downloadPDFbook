07b8d176c961e6cef5f8e30d545d4567fab6cb56
dev eren <dev.erenildo@gmail.com>
2024-06-11 16:36:09 -0300

    connecting mongoDB

---
+++ src/main/java/com/downloadPDFbook/DownloadPdFbookApplication.java
@@ -0,0 +1,13 @@
+package com.downloadPDFbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadPdFbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadPdFbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/downloadPDFbook/DownloadPdFbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.downloadPDFbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadPdFbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
